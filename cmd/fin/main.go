bd3b2fb2b8984d87a433fa6b40a40abf5ccac7e2
Vishnunath A Suresh <vishnunath.suresh06@gmail.com>
2026-01-27 22:15:14 +0530

    Added AST formatter with indented debug output and integrated parser into main CLI

--- cmd/fin/main.go
+++ cmd/fin/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/vishnunath-suresh/fin-project/internal/ast"
 	"github.com/vishnunath-suresh/fin-project/internal/lexer"
+	"github.com/vishnunath-suresh/fin-project/internal/parser"
 )
 
 func main() {
@@ -10,17 +13,20 @@ func main() {
 fn greet name
     set nums [1,2,3]
     set user {name: "bob", age: 20}
-    echo "Hello $name
+    echo "Hello $name"
 end
 `
-
 	l := lexer.New(input)
+	toks := parser.CollectTokens(l)
+	p := parser.New(toks)
+	prog := p.ParseProgram()
 
-	for {
-		tok := l.NextToken()
-		fmt.Printf("%+v\n", tok)
-		if tok.Type == "EOF" {
-			break
+	if errs := p.Errors(); len(errs) > 0 {
+		fmt.Println("Parse errors:")
+		for _, err := range errs {
+			fmt.Println(" -", err)
 		}
 	}
+
+	fmt.Println(ast.Format(prog))
 }
---
+++ internal/ast/print_test.go
@@ -0,0 +1,30 @@
+package ast
+
+import (
+    "strings"
+    "testing"
+)
+
+func TestFormat_ProgramWithStatements(t *testing.T) {
+    prog := &Program{
+        Statements: []Statement{
+            &SetStmt{Name: "x", Value: &NumberLit{Value: "1", P: Pos{Line: 1, Column: 7}}, P: Pos{Line: 1, Column: 1}},
+            &EchoStmt{Value: &IdentExpr{Name: "x", P: Pos{Line: 2, Column: 6}}, P: Pos{Line: 2, Column: 1}},
+        },
+        P: Pos{Line: 1, Column: 1},
+    }
+
+    out := Format(prog)
+    if !containsAll(out, []string{"Program @1:1", "SetStmt name=x @1:1", "NumberLit 1 @1:7", "EchoStmt @2:1", "IdentExpr x @2:6"}) {
+        t.Fatalf("format output missing expected substrings:\n%s", out)
+    }
+}
+
+func containsAll(s string, subs []string) bool {
+    for _, sub := range subs {
+        if !strings.Contains(s, sub) {
+            return false
+        }
+    }
+    return true
+}
