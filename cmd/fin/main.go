9a11d940a1deb173f79d915af3e529fd7e0abbaa
Vishnunath A Suresh <vishnunath.suresh06@gmail.com>
2026-01-30 05:25:33 +0530

    Added .fin extension validation, atomic file writes with temp-then-rename, improved error formatting with multi-error unwrapping, changed usage/unknown-command exit code to 2, added printError/multiError/validateFinPath/atomicWriteFile helpers, and set flag output to stderr

--- cmd/fin/main.go
+++ cmd/fin/main.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"flag"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"os"
 	"path/filepath"
@@ -20,7 +21,7 @@ const version = "0.1.0"
 func main() {
 	if len(os.Args) < 2 {
 		usage()
-		os.Exit(1)
+		os.Exit(2)
 	}
 
 	cmd := os.Args[1]
@@ -37,7 +38,7 @@ func main() {
 	default:
 		fmt.Fprintf(os.Stderr, "unknown command: %s\n", cmd)
 		usage()
-		os.Exit(1)
+		os.Exit(2)
 	}
 }
 
@@ -51,26 +52,31 @@ func usage() {
 
 func buildCmd(args []string) {
 	flags := flag.NewFlagSet("build", flag.ExitOnError)
+	flags.SetOutput(os.Stderr)
 	var outPath string
 	flags.StringVar(&outPath, "o", "", "output batch file")
 	if err := flags.Parse(args); err != nil {
-		os.Exit(1)
+		os.Exit(2)
 	}
 	if flags.NArg() != 1 {
 		fmt.Fprintln(os.Stderr, "build requires exactly one input file")
-		os.Exit(1)
+		os.Exit(2)
 	}
 	inPath := flags.Arg(0)
+	if err := validateFinPath(inPath); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 
 	prog, err := loadAndAnalyze(inPath)
 	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
+		printError(os.Stderr, err)
 		os.Exit(1)
 	}
 
 	out, err := generate(prog)
 	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
+		printError(os.Stderr, err)
 		os.Exit(1)
 	}
 
@@ -78,7 +84,7 @@ func buildCmd(args []string) {
 		base := filepath.Base(inPath)
 		outPath = base[:len(base)-len(filepath.Ext(base))] + ".bat"
 	}
-	if err := ioutil.WriteFile(outPath, []byte(out), 0644); err != nil {
+	if err := atomicWriteFile(outPath, []byte(out), 0644); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
@@ -88,17 +94,21 @@ func buildCmd(args []string) {
 func checkCmd(args []string) {
 	if len(args) != 1 {
 		fmt.Fprintln(os.Stderr, "check requires exactly one input file")
+		os.Exit(2)
+	}
+	if err := validateFinPath(args[0]); err != nil {
+		printError(os.Stderr, err)
 		os.Exit(1)
 	}
 	prog, err := loadAndAnalyze(args[0])
 	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
+		printError(os.Stderr, err)
 		os.Exit(1)
 	}
 
 	// If generate detects unsupported nodes, surface it as an error even in check.
 	if _, err := generate(prog); err != nil {
-		fmt.Fprintln(os.Stderr, err)
+		printError(os.Stderr, err)
 		os.Exit(1)
 	}
 	os.Exit(0)
@@ -107,11 +117,15 @@ func checkCmd(args []string) {
 func astCmd(args []string) {
 	if len(args) != 1 {
 		fmt.Fprintln(os.Stderr, "ast requires exactly one input file")
+		os.Exit(2)
+	}
+	if err := validateFinPath(args[0]); err != nil {
+		printError(os.Stderr, err)
 		os.Exit(1)
 	}
 	prog, err := loadAndAnalyze(args[0])
 	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
+		printError(os.Stderr, err)
 		os.Exit(1)
 	}
 	fmt.Print(ast.Format(prog))
@@ -129,7 +143,7 @@ func loadAndAnalyze(path string) (*ast.Program, error) {
 	p := parser.New(toks)
 	prog := p.ParseProgram()
 	if perrs := p.Errors(); len(perrs) > 0 {
-		return nil, errors.Join(perrs...)
+		return nil, multiError("parse errors", perrs)
 	}
 
 	a := sema.New()
@@ -143,4 +157,56 @@ func loadAndAnalyze(path string) (*ast.Program, error) {
 func generate(prog *ast.Program) (string, error) {
 	g := generator.NewBatchGenerator()
 	return g.Generate(prog)
+}
+
+// printError renders single or joined errors with simple formatting.
+func printError(w io.Writer, err error) {
+	if err == nil {
+		return
+	}
+	type unwrapper interface{ Unwrap() []error }
+	if u, ok := err.(unwrapper); ok {
+		fmt.Fprintln(w, err)
+		for _, e := range u.Unwrap() {
+			fmt.Fprintf(w, " - %v\n", e)
+		}
+		return
+	}
+	fmt.Fprintln(w, err)
+}
+
+func multiError(prefix string, errs []error) error {
+	if len(errs) == 1 {
+		return errs[0]
+	}
+	return fmt.Errorf("%s: %w", prefix, errors.Join(errs...))
+}
+
+func validateFinPath(path string) error {
+	if filepath.Ext(path) != ".fin" {
+		return fmt.Errorf("input must have .fin extension: %s", path)
+	}
+	return nil
+}
+
+// atomicWriteFile writes data to a temp file in the target directory and renames it into place.
+func atomicWriteFile(path string, data []byte, perm os.FileMode) error {
+	dir := filepath.Dir(path)
+	tmp, err := ioutil.TempFile(dir, "fin-*.tmp")
+	if err != nil {
+		return err
+	}
+	tmpPath := tmp.Name()
+	defer os.Remove(tmpPath)
+	if _, err := tmp.Write(data); err != nil {
+		tmp.Close()
+		return err
+	}
+	if err := tmp.Close(); err != nil {
+		return err
+	}
+	if err := os.Chmod(tmpPath, perm); err != nil {
+		return err
+	}
+	return os.Rename(tmpPath, path)
 }
